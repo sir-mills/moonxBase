5f781b1f3b5d757f884ca975f5f63bf7ae419df2
sir-mills <etiosakings@gmail.com>
2024-08-19 02:41:02 -0600

    pause contract function

--- moonx/moonx.sol
+++ moonx/moonx.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
 interface IReputationContract {
     function reputation(address user) external view returns (uint256);
 }
 
-contract DecentralLearning is Ownable {
+contract DecentralLearning is Ownable, Pausable {
     IReputationContract public reputationContract;
     IERC20 public mandToken;
 
@@ -182,6 +183,14 @@ contract DecentralLearning is Ownable {
         require(mandToken.transfer(msg.sender, _amount), "Failed to transfer MAND tokens");
 
         emit CreatorWithdrawal(_courseId, msg.sender, _amount);
+    }
+
+      function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
     }
 
     function updateReputationContract(address _newReputationContract) external onlyOwner {
